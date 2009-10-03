fd6febcd3201a9626f704e50a2536b2433a6104d
roderic <roderic@gmail.com>
2009-10-03 15:27:43 -0700

    now banana peel is added automatically

---
+++ traps/sql/item.sql
@@ -0,0 +1,5 @@
+INSERT INTO "traps_item" VALUES(1,'banana peel',0,'This is a banana peel. It''ll knock you over.',0,'images/bananapeel.png','TP',1,1);
+INSERT INTO "traps_item" VALUES(2,'punching glove',10,'Walk into the room and bamo!!! You got a punched face.',0,'images/glove.jpg','TP',1,1);
+INSERT INTO "traps_item" VALUES(3,'Golden egg of Destiny',3,'Carry this baby and you get 3x your coins when searching',0,'images/GOLDEN-EGG-BRIGHT.jpg','RR',1,1);
+COMMIT;
+
