14691a5bc13ed8cff7864d306a9323ca30e8551a
M. Robert Spryn <bobspryn@MBPSlick.local>
2009-12-02 23:51:15 -0700

    Changing the location of the server to my local machine

--- iphone/Classes/BoobyTrap3AppDelegate.m
+++ iphone/Classes/BoobyTrap3AppDelegate.m
@@ -21,11 +21,11 @@
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application {  
     // Override point for customization after application launch
-#if defined(TARGET_IPHONE_SIMULATOR)
-	[self setServerAddress:@"http://localhost:8000"];
-#else
-	[self setServerAddress:@"http://rodericj.webfactional.com"];
-#endif
+//#if defined(TARGET_IPHONE_SIMULATOR)
+	[self setServerAddress:@"http://10.0.1.195:8000"];
+//#else
+//	[self setServerAddress:@"http://rodericj.webfactional.com"];
+//#endif
 
 	[window addSubview:rootController.view];
 	[window makeKeyAndVisible];
