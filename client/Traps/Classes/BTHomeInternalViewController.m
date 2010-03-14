e40b979f8be7c1a0b9db513c6c05f927ca13976f
Kelvin Kakugawa <kakugawa@Development.local>
2010-03-14 14:55:53 -0700

    remove core data

--- client/Traps/Classes/BTHomeInternalViewController.m
+++ client/Traps/Classes/BTHomeInternalViewController.m
@@ -8,7 +8,6 @@
 
 #import "BTHomeInternalViewController.h"
 
-
 @implementation BTHomeInternalViewController
 
 #pragma mark -
--- client/Traps/Classes/TrapsAppDelegate.h
+++ client/Traps/Classes/TrapsAppDelegate.h
@@ -7,24 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <CoreData/CoreData.h>
 
 #import "BTConstants.h"
 
 @interface TrapsAppDelegate : NSObject <UIApplicationDelegate> {
-
-    NSManagedObjectModel *managedObjectModel;
-    NSManagedObjectContext *managedObjectContext;	    
-    NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
     UIWindow *window;
 }
-
-@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
-@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
-@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-- (NSString *)applicationDocumentsDirectory;
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 
